0e759ea2d772a15cff5623e8158d7bcd5a8a42e6
clearlove-hue <2021536460@qq.com>
2019-11-10 09:02:53 +0800

    11-10test

--- ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,6 +1,125 @@
 ﻿#include<stdio.h>
 #include<string.h>
-//适当使用{  }可以使代码逻辑更清晰
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+for 与while 相比，for更加便捷，因为其三部分较为集中，便于修改
+
+//while循环中break起终止后期循环的作用，continue起终止本次循环的作用
+//int main() {
+//	int i = 1;
+//	while (i <= 10)
+//	{
+//		if (i == 5)
+//           break;/*continue*/
+//		printf("%d", i);
+//		i++;
+//	}
+//	return 0;
+//}
+
+
+//linux面试题1
+//int i;
+//int main(int argc, char* argv[])
+//{
+//	i--;
+//	if (i > sizeof(i))
+//	{
+//		printf(">\n");
+//	}
+//	else
+//	{
+//		printf("<\n");
+//	}
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int day
+//	printf("输入一个小于7的整数")；
+//    scanf_s("")
+// 	
+//	switch (day)
+//	{
+//	case 1:
+//		printf("星期一\n");
+//		break;
+//	case 2:
+//		printf("星期二\n");
+//		break;
+//	case 3:
+//		printf("星期三\n");
+//		break;
+//	case 4:
+//		printf("星期四\n");
+//		break;
+//	case 5:
+//		printf("星期五\n");
+//		break;
+//	case 6:
+//		printf("星期六\n");
+//		break;
+//	case 7:
+//		printf("星期天\n");
+//		break;
+//    default:
+//	   printf("error\n");
+//	}
+//	return 0;
+//}
+
+
+//不完备的时间差计算
+//int main() {
+//	int hour1, minute1;
+//	int hour2, minute2;
+//	scanf_s("%d %d",&hour1,&minute1);
+//	scanf_s("%d %d",&hour2,&minute2);
+//	int ih = hour2 - hour1;
+//	int im = minute2 - minute1;
+//	if (im < 0) {
+//		im = 60 + im;
+//		ih--;
+//	}
+//	printf("时间差是%d小时%d分\n", ih, im);
+//}
+
+
+//struct Stu
+//{
+//	char name[20];
+//	int  age;
+//	char sex[5];
+//	char id[15];
+//};
+//
+//struct Stu s = { "张三"， 20， "男"， "20180101" };
+////.为结构成员访问操作符
+//printf("name = %s age = %d sex = %s id = %s\n", s.name, s.age, s.sex, s.id);
+////->操作符
+//struct Stu* ps = &s;
+//printf("name = %s age = %d sex = %s id = %s\n", ps->name, ps->age, ps->sex, ps->id);
+
+
+
+
+ //适当使用{  }可以使代码逻辑更清晰 ,悬空else问题
 //int main()
 //{
 //	int a = 0;
@@ -8,27 +127,16 @@
 //	if (a == 1)
 //	{
 //		if (b == 2)
-//		{
+//
 //			printf("hehe\n");
-//		 }
-//	 }
+//	}
+//	 
 //	else
 //	{
 //		printf("haha\n");
-//	 }
+//	}
 //	return 0;
 //}
-
-
-
-
-
-
-
-
-
-
-
 
 
 // if语句用法示例         其中if与if else中间的{  }为代码块，若执行多条语句，正常编译即可
@@ -71,17 +179,17 @@
 //};
 //
 //int main() {
-//	struct Stu s = { "李国玉",17,"男","04193129" };
+//	struct stu s = { "liguoyu","17","nan","04193129" };
 //
 //	printf("name=%s age=%d sex=%s id=%s\n", s.name, s.age, s.sex, s.id);
 //
-//	int main(struct Stu* ps = &s);
+//	int main(struct stu* ps = &s);
 //
 //	printf("name=%s age=%d sex=%s id=%s\n", ps->name, ps->age, ps->sex, ps->id);
 //}
 
 
-
+//指针大小
 //int main()
 //{
 //	printf("%d\n", sizeof(char*));//4
@@ -100,12 +208,11 @@
 //	printf("%c\n", ch);
 //	return 0;
 //}
-
-	
-	/*int num = 10;
-	int* p = &num;
-	*p = 20;
-	return 0;*/
+//
+//	int num = 10;
+//	int* p = &num;
+//	*p = 20;
+//	return 0;
 	
 	/*&num;
     printf("%p\n", &num);
