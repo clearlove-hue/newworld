1c967ca336020fd951b46b04ab135287b776a914
clearlove-hue <2021536460@qq.com>
2019-11-19 23:25:27 +0800

    每日一练

---
+++ test_11_19/test_11_19/test_11_19.cpp
@@ -0,0 +1,20 @@
+﻿#include<stdio.h>
+//void PrintN(int N);//输出前N项所有整数
+//void PrintN(int N)
+//{
+//	int i;
+//	printf("结果为:");
+//	for (i = 1; i <= N;i++)
+//		printf("%3d", i);
+//	//return 0;
+//}
+//int main()
+//{
+//	int N;
+//
+//	printf("请输入数字:>");
+//	scanf_s("%d", &N);
+//	PrintN(N);
+//
+//	return 0;
+//}
