c8aa0ed4acc5d39b313105ddabee8bb558b524b9
clearlove-hue <2021536460@qq.com>
2019-12-09 23:08:17 +0800

    提交

---
+++ test_11_22/test_11_22/test_11_22.cpp
@@ -0,0 +1,20 @@
+﻿#include<stdio.h>
+int main()
+{
+	int i, k, n;
+	printf("请输入需要的高度:>");
+	scanf_s("%d", &n);
+	for (i = 1; i <= n; i++)
+	{
+		for (k = 1; k <= n - i; k++)
+		{
+			printf(" ");
+		}
+		for (k = 1; k <= 2 * i - 1; k++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	
+}
